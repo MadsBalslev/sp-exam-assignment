7bef9d1677a9cff426d170ca729d6b818ad6b5c6
Patrick Ritmann Bertelsen <71381584+pberte20@users.noreply.github.com>
2024-06-07 13:23:54 +0200

    2 more examples

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ using namespace SpStochLib::Examples;
 
 int main() {
     Simulation example1 = simple();
+    Simulation example2 = simple(100,0,2);
+    Simulation example3 = simple(50,50,1);
 
     example1.simulate(2000, std::nullopt);
+    example2.simulate(2000, std::nullopt);
+    example3.simulate(2000, std::nullopt);
 
     return 0;
 }
