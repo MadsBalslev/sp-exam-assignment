9c424da3f313a0e6bf8a1c72790a96741dfa19e0
Patrick Ritmann Bertelsen <pberte20@student.aau.dk>
2024-06-16 00:24:29 +0200

    Update main.cpp

--- estimation/main.cpp
+++ estimation/main.cpp
@@ -10,8 +10,8 @@ int main() {
     auto N_DK =  5822763;
     auto N_NJ = 589755;
 
-    auto DkResult = estimatePeakMean(N_DK, 100);
-    auto NjResult = estimatePeakMean(N_NJ, 100);
+    auto DkResult = estimatePeakMean(N_DK, 100, 1);
+    auto NjResult = estimatePeakMean(N_NJ, 100, 1);
 
     std::cout << "Peak DK hospitalized: " << DkResult.first << std::endl;
     std::cout << "Mean DK hospitalized: " << DkResult.second << std::endl;
